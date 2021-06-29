4e6b3eaea435bfed3ab8cf4032e6e1ac80c0279a
Ankur <ankursolanki103055555@gmail.com>
2021-06-29 16:23:59 +0530

    fouth commit

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,29 @@
 import Paper from '@material-ui/core/Paper';
-import {useEffect} from 'react'
-import router from 'next/router'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
-export default function Home() {
-
-useEffect(()=>{
-  router.push('/portfolios')
-},[])
+export default function Home({user}) {
 
   return (<>
-    <Paper variant="outlined" style={{height:"100em",backgroundColor:"green"}}>
-      <h1>WELCOME You</h1>
+    <Paper variant="outlined" style={{height:"43em",backgroundColor:"#F7DBF0"}}>
+      <Card style={{marginTop:"13%",marginLeft:"33%",maxWidth:"30em",backgroundColor:"#CDF0EA"}}>
+        <CardContent>
+          <Typography style={{marginLeft:"20%"}}>
+            {user?<h3>Welcome {user.name}</h3>:<h1>Welcome User</h1>}
+          </Typography>
+        </CardContent>
+        {!user ? <CardContent>
+          <Typography style={{marginLeft:"7%"}}>
+            <h3>Please Login to Ask or to Answer question</h3>
+          </Typography>
+        </CardContent>:<CardContent>
+          <Typography style={{marginLeft:"7%"}}>
+            <h3>Share Your Question with others or Help someone by answering their questions</h3>
+          </Typography>
+        </CardContent>}
+      </Card>
     </Paper>
-    
-  </>
-        
+  </>   
   )
 }
--- pages/portfolios/new.js
+++ pages/portfolios/new.js
@@ -3,6 +3,7 @@ import {usePostQuestion} from 'actions/postQuestion'
 import {useUser} from '@auth0/nextjs-auth0';
 import { useEffect } from 'react';
 import {useRouter} from 'next/router'
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 //import {useRouter} from 'next/router'
 
 const NewQuestion = () =>{
@@ -24,3 +25,5 @@ const [postQuestion,{data,error,loading}] = usePostQuestion()
 }
 
 export default NewQuestion
+
+export const getServerSideProps = withPageAuthRequired();
